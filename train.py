76b06ea6c2f647da7c07a1baa58334f7c25f4129
Redha Moulla <151394880+RMoulla@users.noreply.github.com>
2026-06-09 07:17:24 +0200

    Refactor model training to use a pipeline

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd   
 import numpy as np 
 from sklearn.linear_model import LogisticRegression  
+from sklearn.pipeline import Pipeline
 import joblib                      
        
 # Charger les données                                        
@@ -12,13 +13,17 @@ X = data[['Age', 'Account_Manager', 'Years', 'Num_Sites']]
 # Sélectionner la colonne cible
 y = data['Churn']
 
-# Créer et entraîner le modèle de régression logistique
-model = LogisticRegression()
+# Entraîner le modèle de régression logistique
+pipeline = Pipeline([
+    ('scaler', StandardScaler()),
+    ('logit', LogisticRegression())
+])
 
-# Entraîner le modèle sur les données
-model.fit(X, y)
+pipeline.fit(X_train, y_train)
+
+
 
 # Sauvegarder le modèle entraîné avec joblib (sans dépendances pandas)
-joblib.dump(model, 'data/churn_model_clean.pkl')
+joblib.dump(pipeline, 'data/churn_model_clean.pkl')
 
 print("Modèle de régression logistique entraîné et sauvegardé sous 'churn_model_clean.pkl'.")
